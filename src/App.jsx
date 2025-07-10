cfc726effc6e939054eddb16306b6b8ee15bcb7b
ParindaLert020 <parinda.lert@mail.kmutt.ac.th>
2025-07-10 16:25:20 +0700

    Start Learning React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <>  
+      <h1>Hello World!!!</h1>
+      <p>React Crash Course</p>
+    </>
+  );
+};
+
+export default App;
