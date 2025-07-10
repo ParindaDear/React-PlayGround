d2499d3e8ea292e7b63443f1327661e7b9e085d5
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-07-10 22:01:43 +0700

    lesson  : set up basic routing with default  home page (ตั้งค่าเกี่ยวกับหน้าเเรกเมื่อเปิด web มา)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom"
+
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-  <>
-    <NavBar/>
-    <main>Hello Dear!!!</main>
-    <Footer/>
-  </>
-  )
+
+  // การใช้คำว่า index จะทำให้สิ่งนี้เเสดงเป็นหน้าเเรกที่เปิด web ขึ้นมา
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={ <h1>Hello World!!!</h1> } />)
+  );
+
+  return <RouterProvider router={router}/>;
 };
 
 export default App;
