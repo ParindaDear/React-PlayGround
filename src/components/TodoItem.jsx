f2c64fdd04155cb789a9aef9e521ec3a479b5af7
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-07-12 00:05:52 +0700

    lesson : Using icons, we will create, delete and edit icons, which can be done using react libraries

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,30 @@
-
+import { MdDelete, MdEdit } from "react-icons/md";
 
 const TodoItem = (props) => {
   return (
-    <li className="flex bg-white rounded shadow-sm p-4 mt-4 first:mt-0">
-        <div className="flex gap-x-4 mr-auto items-center">
-            <div className="h-6 w-6 rounded-full shadow-small text-white text-sm bg-teal-400 text-center content-center">
-                {props.id + 1}
+    <>
+        <li className="flex bg-white rounded shadow-sm p-4 mt-4 first:mt-0">
+            <div className="flex gap-x-4 mr-auto items-center">
+                <div className="h-6 w-6 rounded-full shadow-small text-white text-sm bg-teal-400 text-center content-center">
+                    {props.id + 1}
+                </div>
+                <div>
+                    <p className="font-semibod">{props.todo.title}</p>
+                    <p className="text-sm text-gray-400">{props.todo.date}</p>
+                </div>
             </div>
-            <div>
-                <p className="font-semibod">{props.todo.title}</p>
-                <p className="text-sm text-gray-400">{props.todo.date}</p>
-            </div>
-        </div>
-    </li>
-  )
-}
 
-export default TodoItem
+            <div className="flex items-center gap-x-2">
+                <button type="button" className="todo-btn">
+                    <MdDelete/>
+                </button>
+                <button type="button" className="todo-btn">
+                    <MdEdit/>
+                </button>
+            </div>
+        </li>
+    </>
+  );
+};
+
+export default TodoItem;
