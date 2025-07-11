8fd5bc7e33bddaef603260b520b3b9184e32536b
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-07-11 16:03:27 +0700

    lesson : Add custom 404 error page and route fallback handling with React Router(ตกแต่งนหน้า error)

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} fr
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
 
   // การใช้คำว่า index จะทำให้สิ่งนี้เเสดงเป็นหน้าเเรกที่เปิด web ขึ้นมา
   // MainLayout ให้ครอบทุกอย่าง
+  // ใส่ path ของ NotFoundPage เป็น * เพราะสื่อว่าจับทุกเส้นทางที่ไม่ตรงกับ route อื่น ๆ
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout/>}>
         <Route index element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
   );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+  return (
+    <div className="text-center">
+        <h1 className="text-gray-300 font-bold text-7xl">404 ERROR
+        <h2 className="mt-6 font-bold text-3xl">Page Not Found</h2>
+        <p className="text-xl">The page ypu are lookin for does not exist.</p>
+        </h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
